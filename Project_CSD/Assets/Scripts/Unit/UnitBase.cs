aaa1bc2580b01256229d40084fde89b62eb7d9a5
Ori <uic3883@gmail.com>
2024-03-06 16:47:32 +0900

    Update UnitBase.cs

--- Project_CSD/Assets/Scripts/Unit/UnitBase.cs
+++ Project_CSD/Assets/Scripts/Unit/UnitBase.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class UnitBase : MonoBehaviour
 {
-    public enum UnitState { Idle, Move, Fight, Attack, Damaged, Die}
+    public enum UnitState { Idle, Move, Fight, Attack, Hit, Die}
 
     [Header("# UnitState")]
     public UnitState unitState;
