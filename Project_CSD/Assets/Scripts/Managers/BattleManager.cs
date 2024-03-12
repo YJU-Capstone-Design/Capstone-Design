40e595da028e58c3013ba502e23285d33c1d1713
Peinoi <124995489+Peinoi@users.noreply.github.com>
2024-03-12 22:16:20 +0900

    hpbar Add

--- Project_CSD/Assets/Scripts/Managers/BattleManager.cs
+++ Project_CSD/Assets/Scripts/Managers/BattleManager.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class BattleManager : MonoBehaviour
 {
-    [SerializeField] private float hpBar;
+    [Header("HpVar")]
+    [SerializeField] private float curHealth; //* 현재 체력
+    [SerializeField] private float maxHealth; //* 최대 체력
+    public Slider HpBarSlider;
 
-
+    private void Awake()
+    {
+        curHealth = maxHealth;
+    }
+    private void Update()
+    {
+        Invoke("HpCheck", 3f);
+    }
+    public void HpCheck()
+    {
+        curHealth -= 3f;
+        HpBarSlider.value = curHealth;
+    }
 
 }
