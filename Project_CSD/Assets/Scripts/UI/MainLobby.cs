569fba88e06af5bbde734020628d7642ce470356
Peinoi <ailis635@naver.com>
2024-03-19 18:52:59 +0900

    MainLobby
    메인로비 토글 닫고 열기 완성
    메인에서 가챠, 주방, 트레이닝 이동 고민중 씬or오브젝트 setactive

---
+++ Project_CSD/Assets/Scripts/UI/MainLobby.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainLobby : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> menu = new List<GameObject>();
+    [SerializeField] private TextMeshProUGUI toggle;
+    [SerializeField] private GameObject toggleMenu;
+
+    [Header("toggle Transform")]
+    [SerializeField] private GameObject toggleBtn;
+    [SerializeField] private GameObject closeToggleBtn;
+    private Vector3 openToggleTr;//토클의 처음 위치
+    private Vector3 closeToggleTr;//토클이 닫혔을 때의 위치
+   
+
+    private void Awake()
+    {
+        Clear();
+        openToggleTr = toggleBtn.transform.localPosition;
+        closeToggleTr = closeToggleBtn.transform.localPosition;
+        
+    }
+
+    public void OpenScene(string type)
+    {
+        int openScene = 0;
+        if(type == "Gacha")
+        {
+            openScene = 1;
+        }else if(type == "MyCard")
+        {
+            openScene = 2;
+        }
+        else if (type == "Formation")
+        {
+            openScene = 3;
+        }
+        else if (type == "TraningBtn")
+        {
+            openScene = 4;
+        }
+        else if (type == "Collection")
+        {
+            openScene = 5;
+        }
+        else if (type == "Chm&Mission")
+        {
+            openScene = 6;
+        }
+        foreach(GameObject go in menu)
+        {
+            go.SetActive(false);
+
+        }
+        menu[openScene].SetActive(true);
+    }
+
+
+    public void ToggleOnOff()
+    {
+        if(toggle.text == "<")
+        {
+            toggleMenu.SetActive(false);
+            toggle.text = ">";
+            
+            toggleBtn.transform.localPosition = closeToggleTr;
+        }
+        else
+        {
+            toggleMenu.SetActive(true);
+            toggle.text = "<";
+            toggleBtn.transform.localPosition = openToggleTr;
+        }
+    }
+
+
+    void Clear()
+    {
+        menu[0].gameObject.SetActive(true);
+        for(int i=1; i<menu.Count; i++)
+        {
+            menu[i].gameObject.SetActive(false);
+        }
+    }
+}
