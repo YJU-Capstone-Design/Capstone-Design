dbbe418c08fdf9cbc387bd2c93d3934c1c9a39ee
Peinoi <ailis635@naver.com>
2024-05-28 20:41:35 +0900

    goldadd

--- Project_CSD/Assets/Scripts/Managers/CashManager.cs
+++ Project_CSD/Assets/Scripts/Managers/CashManager.cs
@@ -6,14 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class CashManager : MonoBehaviour
 {
+    public CashManager instance;
     [Header("Money")]
     public int player_Gold;
     public int player_Cash;
     [SerializeField] private TextMeshProUGUI GoldText;
     [SerializeField] private TextMeshProUGUI CashText;
+    public GameObject obj1;
     // Start is called before the first frame update
     private void Awake()
     {
+        instance = this;
         var obj = FindObjectsOfType<CashManager>();
 
 
@@ -26,13 +29,42 @@ public class CashManager : MonoBehaviour
             Destroy(gameObject);
         }
 
-        GoldText.text = player_Gold.ToString();
-        CashText.text = player_Cash.ToString();
+        
     }
 
     public void Update()
     {
-        GameObject obj1 = GameObject.Find("Cash (TMP)");
+        if(obj1 == null)
+        {
+            obj1 = GameObject.Find("Cash (TMP)");
+            Debug.Log(obj1.gameObject.ToString());
+            
+        }
+        
+        UpGold();
+
+    }
+    public void UpGold()
+    {
+        if (obj1 != null)
+        {
+            // TextMeshPro 컴포넌트를 가져옵니다.
+            TextMeshProUGUI tmp = obj1.GetComponent<TextMeshProUGUI>();
+            string a = player_Gold.ToString();
+            if (!tmp.Equals(a))
+            {
+                // 텍스트 값을 변경합니다.
+                if (tmp != null)
+                {
+                    tmp.text = player_Gold.ToString();
+                }
+                else
+                {
+                    Debug.LogError("TextMeshProUGUI 컴포넌트를 찾을 수 없습니다.");
+                }
+            }
+        }
+
     }
 
     public void PlusGold()
---
+++ Project_CSD/Assets/Scripts/UI/Gold.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold : MonoBehaviour
+{
+    public Transform gold;
+
+    private void Awake()
+    {
+       
+    }
+}
