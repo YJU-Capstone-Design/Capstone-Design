5f63cbfb7566fb9085b3899c2b90e7a84fc2660c
Ori-hub-code <uic3883@gmail.com>
2024-05-28 21:58:32 +0900

    제작된 Unit 들 hp 바 위치 조정

--- Project_CSD/Assets/Scripts/Unit/HpBar.cs
+++ Project_CSD/Assets/Scripts/Unit/HpBar.cs
@@ -26,7 +26,19 @@ public class HpBar : MonoBehaviour
                 hpBarPos = new Vector3((hpBarDir.x < 0 ? -0.2f : 0f), 3.5f);
                 break;
             case string name when name.Contains("Skeleton") || name.Contains("Zombie"):
-                hpBarPos = new Vector3((hpBarDir.x < 0 ? -0.2f : 0f), 1.8f);
+                hpBarPos = new Vector3((hpBarDir.x < 0 ? -0.1f : 0f), 1.8f);
+                break;
+            case string name when name.Contains("Slime"):
+                hpBarPos = new Vector3((hpBarDir.x < 0 ? 0f : -0.1f), 1.3f);
+                break;
+            case string name when name.Contains("WhiteBread"):
+                hpBarPos = new Vector3((hpBarDir.x < 0 ? -0.2f : 0f), 2f);
+                break;
+            case string name when name.Contains("Ramo"):
+                hpBarPos = new Vector3((hpBarDir.x < 0 ? -0.4f : -0.1f), 1.8f);
+                break;
+            case string name when name.Contains("Pupnut"):
+                hpBarPos = new Vector3(-0.05f, 1.5f);
                 break;
             default:
                 hpBarPos = new Vector3((hpBarDir.x < 0 ? -0.2f : 0f), 1.5f);
