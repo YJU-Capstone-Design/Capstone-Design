2afc9ae7a1951783383639a79ba72b0e021b77cf
sun88110 <138852390+sun88110@users.noreply.github.com>
2024-06-11 16:10:42 +0900

    Round UI script

---
+++ Project_CSD/Assets/Temp/Scripts/UI/RoundUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundUI : MonoBehaviour
+{
+    float radius = 320f;
+
+    void Update()
+    { 
+        int numOfObjects = 3; // 배치할 자손 오브젝트의 수
+        float angleIncrement = 180f / (numOfObjects + 1); // 반원 내에서 각 자손 오브젝트의 간격
+
+        for (int i = 0; i < numOfObjects; i++)
+        {
+            float angle = (i + 1) * angleIncrement; // 반원 내에서 자손 오브젝트의 각도
+
+            Transform child = transform.GetChild(i); // i번째 자손 오브젝트 가져오기
+
+            // 각도를 라디안으로 변환하여 코사인과 사인 값을 계산하여 위치를 설정합니다.
+            float radianAngle = angle * Mathf.Deg2Rad;
+            child.localPosition = new Vector2(Mathf.Cos(radianAngle), Mathf.Sin(radianAngle)) * radius;
+        }
+    }
+}
