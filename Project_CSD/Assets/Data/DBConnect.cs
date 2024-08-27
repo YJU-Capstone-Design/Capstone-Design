d5459f8ea06fef50d21c2fd94ff69dbe3da03d18
Ori <uic3883@gmail.com>
2024-08-27 15:53:33 +0900

    DB 연결 테스트 중

---
+++ Project_CSD/Assets/Data/DBConnect.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+using System;
+
+public class DBConnect : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Connection Test :" + Connection());
+    }
+
+
+    public bool Connection()
+    {
+        string conStr = string.Format("Server={0};Database={1};Uid={2};Pwd={3};", "127.0.0.1", "gameresult", "root", "1478");
+
+        try
+        {
+            using(MySqlConnection con = new MySqlConnection(conStr))
+            {
+                con.Open();
+            }
+            return true;
+        }
+        catch(Exception e)
+        {
+            Debug.Log("e : " + e.ToString());
+            return false;
+        }
+    }
+}
