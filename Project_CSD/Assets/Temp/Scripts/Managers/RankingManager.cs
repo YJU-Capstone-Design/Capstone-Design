80c50b847edded8fde9489ac898364a6434bfb0a
Peinoi <ailis635@naver.com>
2024-09-04 23:00:59 +0900

    ranking
    3분마다 ranking초기화

--- Project_CSD/Assets/Temp/Scripts/Managers/RankingManager.cs
+++ Project_CSD/Assets/Temp/Scripts/Managers/RankingManager.cs
@@ -7,10 +7,30 @@ public class RankingManager : MonoBehaviour
 {
     [SerializeField] Transform creatRanking_Tr; // 랭킹 프리팹 생성 위치
     [SerializeField] GameObject ranking_Obj; // 랭킹 아이템 프리팹
-    
+
+    float time;
+
     private void Start()
     {
-      
+        RankingSystem();
+
+    }
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= 180)
+        {
+            foreach (Transform child in creatRanking_Tr)
+            {
+                Destroy(child.gameObject);
+            }
+            RankingSystem();
+            time = 0f;
+        }
+    }
+
+    void RankingSystem()
+    {
         // 데이터베이스에서 1위부터 10위까지의 랭킹 데이터를 가져옴
         XmlNodeList rankingData = DBConnect.Instance.Select("ranking", "ORDER BY time ASC LIMIT 10");
 
