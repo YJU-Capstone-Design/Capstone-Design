a5d3dd0adbb9a8990e37623fa2c1e88bfa4a5d13
Peinoi <ailis635@naver.com>
2024-09-09 15:10:34 +0900

    StartScenePlayerData_Save
    시작씬에서 플레이어 로그인 및 랭킹 데이터 관련 변수 저장

---
+++ Project_CSD/Assets/Temp/Scripts/UI/Start/UserRankingData.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserRankingData : MonoBehaviour
+{
+    public static UserRankingData instance;
+
+    [Header("유저 데이터 랭킹 변수")]
+    public int stage = 1; //현재 스테이지
+    public int clear_Stage = 1; // 클리어한 스테이지
+    public int score;
+    
+    [Header("플레이어 정보")]
+    [SerializeField] TMP_InputField player_Name;
+    [SerializeField] TMP_InputField player_No;
+    [SerializeField] GameObject loginBox;//로그인 창
+    public string playerName;//이름
+    public int playerNo;//학번
+
+    private void Awake()
+    {
+        instance = this;
+        var obj = FindObjectsOfType<UserRankingData>();
+
+        if (obj.Length == 1)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        loginBox.SetActive(false); 
+    }
+
+
+
+
+    public void Login()
+    {
+        if (player_Name != null && player_No !=null)
+        {
+            if (AudioManager.instance != null) { AudioManager.instance.ButtonSound(); }
+            playerName = player_Name.text;
+            playerNo = int.Parse(player_No.text);
+            
+            SceneMgr.Instance.GoSceneSelect("NomalMode");
+        }
+    }
+
+    public void Close()//로그인 창 나가기
+    {
+        if (AudioManager.instance != null) { AudioManager.instance.ButtonSound(); }
+        loginBox.SetActive(false);
+    }
+    public void OpenLogin()
+    {
+        if (AudioManager.instance != null) { AudioManager.instance.ButtonSound(); }
+        loginBox.SetActive(true);
+    }
+
+}
