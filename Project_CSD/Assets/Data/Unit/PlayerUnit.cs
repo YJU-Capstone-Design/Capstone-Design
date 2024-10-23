e9a445213e9e16ab833f8c9648afe9f71e03a644
Peinoi <ailis635@naver.com>
2024-10-23 09:24:31 +0900

    froll
    속도 수정

--- Project_CSD/Assets/Data/Unit/PlayerUnit.cs
+++ Project_CSD/Assets/Data/Unit/PlayerUnit.cs
@@ -382,7 +382,7 @@ public class PlayerUnit : UnitBase
         if(unitData.UnitID == 11007)//개구리
         {
             StartAnimation("Attack", false, 1f);
-            yield return new WaitForSeconds(1.157f);
+            yield return new WaitForSeconds(1.167f);
         }
         // 일반 유닛 공격 애니메이션
         if (unitData.UnitID != 11005 && unitData.UnitID != 11006 && unitData.UnitID != 11007)
